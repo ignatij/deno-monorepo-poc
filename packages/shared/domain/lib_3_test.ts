762be03e6f33dc56338864e21b187bc88c79e1b7
mathieueveillard <mathieu.eveillard@gmail.com>
2025-08-25 17:28:09 +0200

    CI: test with 5000 unit tests

---
+++ packages/shared/domain/lib_3_test.ts
@@ -0,0 +1,93 @@
+import { assertEquals } from "@std/assert";
+import { randomIntegerBetween } from "@std/random";
+import { add } from "./lib.ts";
+
+for (let i = 0; i < 50; i++) {
+  Deno.test(function addTest() {
+    const a = randomIntegerBetween(-10000000, 10000000);
+    const b = randomIntegerBetween(-10000000, 10000000);
+    const c = a + b;
+    assertEquals(add(a, b), c);
+  });
+}
+
+for (let i = 0; i < 50; i++) {
+  Deno.test(function addTest() {
+    const a = randomIntegerBetween(-10000000, 10000000);
+    const b = randomIntegerBetween(-10000000, 10000000);
+    const c = a + b;
+    assertEquals(add(a, b), c);
+  });
+}
+
+for (let i = 0; i < 50; i++) {
+  Deno.test(function addTest() {
+    const a = randomIntegerBetween(-10000000, 10000000);
+    const b = randomIntegerBetween(-10000000, 10000000);
+    const c = a + b;
+    assertEquals(add(a, b), c);
+  });
+}
+
+for (let i = 0; i < 50; i++) {
+  Deno.test(function addTest() {
+    const a = randomIntegerBetween(-10000000, 10000000);
+    const b = randomIntegerBetween(-10000000, 10000000);
+    const c = a + b;
+    assertEquals(add(a, b), c);
+  });
+}
+
+for (let i = 0; i < 50; i++) {
+  Deno.test(function addTest() {
+    const a = randomIntegerBetween(-10000000, 10000000);
+    const b = randomIntegerBetween(-10000000, 10000000);
+    const c = a + b;
+    assertEquals(add(a, b), c);
+  });
+}
+
+for (let i = 0; i < 50; i++) {
+  Deno.test(function addTest() {
+    const a = randomIntegerBetween(-10000000, 10000000);
+    const b = randomIntegerBetween(-10000000, 10000000);
+    const c = a + b;
+    assertEquals(add(a, b), c);
+  });
+}
+
+for (let i = 0; i < 50; i++) {
+  Deno.test(function addTest() {
+    const a = randomIntegerBetween(-10000000, 10000000);
+    const b = randomIntegerBetween(-10000000, 10000000);
+    const c = a + b;
+    assertEquals(add(a, b), c);
+  });
+}
+
+for (let i = 0; i < 50; i++) {
+  Deno.test(function addTest() {
+    const a = randomIntegerBetween(-10000000, 10000000);
+    const b = randomIntegerBetween(-10000000, 10000000);
+    const c = a + b;
+    assertEquals(add(a, b), c);
+  });
+}
+
+for (let i = 0; i < 50; i++) {
+  Deno.test(function addTest() {
+    const a = randomIntegerBetween(-10000000, 10000000);
+    const b = randomIntegerBetween(-10000000, 10000000);
+    const c = a + b;
+    assertEquals(add(a, b), c);
+  });
+}
+
+for (let i = 0; i < 50; i++) {
+  Deno.test(function addTest() {
+    const a = randomIntegerBetween(-10000000, 10000000);
+    const b = randomIntegerBetween(-10000000, 10000000);
+    const c = a + b;
+    assertEquals(add(a, b), c);
+  });
+}
