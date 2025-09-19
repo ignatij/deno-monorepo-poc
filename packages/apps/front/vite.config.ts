7e8fa970e1407cccd208fdccff269e460f7fd383
mathieueveillard <mathieu.eveillard@gmail.com>
2025-09-19 09:58:12 +0200

    test -> front + deno-vite-plugin

--- packages/apps/front/vite.config.ts
+++ packages/apps/front/vite.config.ts
@@ -1,27 +1,8 @@
 import { defineConfig } from "vite";
+import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@deno-monorepo-poc/domain": path.resolve(
-        path.dirname(new URL(import.meta.url).pathname),
-        "../../shared/domain/lib.ts"
-      ),
-    },
-  },
-  server: {
-    fs: {
-      allow: [
-        path.resolve(
-          path.dirname(new URL(import.meta.url).pathname),
-          "../../shared/domain"
-        ),
-        path.resolve(path.dirname(new URL(import.meta.url).pathname)),
-      ],
-    },
-  },
+  plugins: [react(), deno()],
 });
