76330ba6875cbc1ea4f57113ab0ff9acd7dc7d5c
Ignatij Gichevski <ignatij.gichevski@gmail.com>
2025-10-10 17:06:04 +0200

    chore: add update_version script

---
+++ packages/apps/modular-monolith/scripts/update_version.ts
@@ -0,0 +1,6 @@
+const file = "deno.json";
+const version = Deno.args[0];
+const json = JSON.parse(await Deno.readTextFile(file));
+json.version = version;
+await Deno.writeTextFile(file, JSON.stringify(json, null, 2));
+console.log(`✅ Updated deno.json to ${version}`);
