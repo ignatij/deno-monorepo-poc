32dd340ea50ea404f2043168ba287f01e6111f8c
Ignatij Gichevski <ignatij.gichevski@gmail.com>
2025-10-14 09:59:32 +0200

    chore: rollback to node script on front

---
+++ packages/apps/front/scripts/update_version.js
@@ -0,0 +1,8 @@
+import fs from "node:fs";
+import process from "node:process";
+
+const version = process.argv[2];
+const json = JSON.parse(fs.readFileSync("package.json", "utf8"));
+json.version = version;
+fs.writeFileSync("package.json", JSON.stringify(json, null, 2));
+console.log(`✅ Updated package.json to ${version}`);
--- packages/apps/front/scripts/update_version.ts
+++
@@ -1,6 +0,0 @@
-const file = "package.json";
-const version = Deno.args[0];
-const json = JSON.parse(await Deno.readTextFile(file));
-json.version = version;
-await Deno.writeTextFile(file, JSON.stringify(json, null, 2));
-console.log(`✅ Updated package.json to ${version}`);
