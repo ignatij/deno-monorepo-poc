091989a0ca8706ced572933f34f77da1b75b940f
Ignatij Gichevski <ignatij.gichevski@gmail.com>
2025-10-24 18:36:28 +0200

    fix: improve release script

--- packages/apps/modular-monolith/scripts/release.js
+++ packages/apps/modular-monolith/scripts/release.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
-import semanticRelease from 'semantic-release';
+import semanticRelease from "semantic-release";
 
 const result = await semanticRelease({
   ci: true,
-  config: './packages/apps/modular-monolith/.releaserc.json'
+  config: "./packages/apps/modular-monolith/.releaserc.json",
 });
 
 if (!result) {
-  console.error('No release triggered');
-  process.exitCode = 1;
+  console.error("No release triggered");
 }
