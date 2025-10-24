653832cb90194722eaf9f10dff809d29aef7d8db
Ignatij Gichevski <ignatij.gichevski@gmail.com>
2025-10-24 18:27:19 +0200

    chore: rollback to node and convention commits

---
+++ packages/apps/modular-monolith/scripts/release.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import semanticRelease from 'semantic-release';
+
+const result = await semanticRelease({
+  ci: true,
+  config: './packages/apps/modular-monolith/.releaserc.json'
+});
+
+if (!result) {
+  console.error('No release triggered');
+  process.exitCode = 1;
+}
