fe9a7e4b52862853a71cebc714cc363de6de15e7
Ignatij Gichevski <ignatij.gichevski@gmail.com>
2025-10-27 11:05:18 +0100

    fix(front): fix

--- packages/apps/front/src/App.tsx
+++ packages/apps/front/src/App.tsx
@@ -7,7 +7,7 @@ await FeatureConfig.initialize();
 const config = FeatureConfig.getInstance().getFeatureFlags();
 
 const App: React.FunctionComponent = () => {
-  const a = 3;
+  const a = 2;
   const b = 2;
   const c = add(a, b);
 
